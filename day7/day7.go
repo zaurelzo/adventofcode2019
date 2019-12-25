247c21c734044034ebb68c3bf61a91ccd3887f76
zaurelzo <zaurelzo@users.noreply.github.com>
2019-12-25 21:54:17 +0100

    OMG : day 7 part 2 was a nightmare :   1) the explanations were not clear.   2) take me so many hours to implement the feedback loop using goroutine,      but I learn a lot about go channel, synchronization and communicate between them.  3) sometimes, a deadlock appears in that code for part 2, I don't know why and I will try to fix it later (but the code gives the right answer)

--- day7/day7.go
+++ day7/day7.go
@@ -5,7 +5,7 @@ import (
 	"strconv"
 )
 
-func diagnosticCode(programCode []int, inputChan chan int, outputChan chan int) ([]int, error) {
+func diagnosticCode(programCode []int, inputChan chan int, outputChan chan int, haltChan chan bool, amplifierid int) /*([]int, error)*/ {
 	printedValue := make([]int, 0)
 	output := make([]int, len(programCode))
 	copy(output, programCode)
@@ -27,13 +27,15 @@ func diagnosticCode(programCode []int, inputChan chan int, outputChan chan int) 
 		case 3:
 			//use golang chan to read input. Since reading from chan is a blocking operation
 			//that feed perfectly the definition of blocking input describe by the challenge to read the signal input
+			//fmt.Printf(" waiting %d... \n", amplifierid)
 			inputValue := <-inputChan
 			output[output[instructionPointer+1]] = inputValue
 			instructionPointer += 2
 		//print output
 		case 4:
 			printedValue = append(printedValue, getValue(output, instructionPointer+1, instAsByte[2]))
-			//fmt.Printf("%d\n", getValue(output, instructionPointer+1, instAsByte[2]))
+			//send  output signal
+			outputChan <- printedValue[len(printedValue)-1]
 			instructionPointer += 2
 		//jump if true
 		case 5:
@@ -70,15 +72,16 @@ func diagnosticCode(programCode []int, inputChan chan int, outputChan chan int) 
 		//halt
 		case 99:
 			halt = true
+
 		//instruction not supported
 		default:
-			return []int{}, fmt.Errorf("WARN, UNKNOWN INSTRUCTION %+v, extracted from %+v at index %+v ", instCode, output[instructionPointer], instructionPointer)
-			//panic("unknow instruction")
+			//return []int{}, fmt.Errorf("WARN, UNKNOWN INSTRUCTION %+v, extracted from %+v at index %+v ", instCode, output[instructionPointer], instructionPointer)
+			panic("unknow instruction")
 		}
 	}
-	//send  output signal
-	outputChan <- printedValue[len(printedValue)-1]
-	return printedValue, nil
+	//fmt.Printf(" halt ? %v, id %d \n", halt, amplifierid)
+	//does the interpreter halt ?
+	haltChan <- halt
 }
 
 //transform an instruction to follow the pattern ABCDE, with A being optional
@@ -142,36 +145,51 @@ func computeAmplifiersValue(programCode []int, phaseSettingValues []int) int {
 	amplifierValues = append(amplifierValues, 0)
 	//use chan to simulate signal input and output of amplifier
 	//each amplifier is represented by a goroutine and the signal mechanism is simulate by chan
-	//a output chan of amplifier is the input chan of the next amplifier
+	//an output value of the current chan is the input value of the next amplifier input chan
 	//Because reading from a chan and writing to a chan block when the other side of the chan is not
 	//ready, this mechanism naturaly synchronise amplifiers (the first amplifier execute completely,
 	// then the second amplifer and son on)
 	var arrayInputOutputChan []pairOfChan = initializePairOfChan(len(phaseSettingValues))
-	for ampNumber, phaseSetting := range phaseSettingValues {
-		go diagnosticCode(programCode, arrayInputOutputChan[ampNumber].inputChan, arrayInputOutputChan[ampNumber].outputChan)
-		//provide input signal of the current amplifier
-		arrayInputOutputChan[ampNumber].inputChan <- phaseSetting
-		arrayInputOutputChan[ampNumber].inputChan <- amplifierValues[len(amplifierValues)-1]
+	for ampID, phaseSetting := range phaseSettingValues {
+		//launch amplifier
+		go diagnosticCode(programCode, arrayInputOutputChan[ampID].inputChan, arrayInputOutputChan[ampID].outputChan,
+			arrayInputOutputChan[ampID].outputHaltChan, ampID)
+		//provide input signal to the current amplifier
+		arrayInputOutputChan[ampID].inputChan <- phaseSetting
+		arrayInputOutputChan[ampID].inputChan <- amplifierValues[len(amplifierValues)-1]
 		//get the output signal of the current amplifier and save this value the use it as the input signal of the next
 		//amplifier
-		amplifierValues = append(amplifierValues, <-arrayInputOutputChan[ampNumber].outputChan)
+		amplifierValues = append(amplifierValues, <-arrayInputOutputChan[ampID].outputChan)
+
+		//don't care if the amplifier halt or not, put this code to not have a deadlock (ie : the
+		//amplifier send a halt value and we don't read it)
+		select {
+		case <-arrayInputOutputChan[ampID].outputHaltChan:
+		default:
+			// pass throught to not block when no halt value is sended over arrayInputOutputChan[ampID].outputHaltChan
+		}
+
 	}
 	return amplifierValues[len(amplifierValues)-1]
 }
 
 type pairOfChan struct {
-	inputChan  chan int
+	//reading input signal
+	inputChan chan int
+	//sending output signal
 	outputChan chan int
+	//to now if the interpreter code has halt or not
+	outputHaltChan chan bool
 }
 
 func initializePairOfChan(nbOfElement int) []pairOfChan {
 	array := make([]pairOfChan, nbOfElement)
 	for i := 0; i < nbOfElement; i++ {
-		if i == 0 {
-			array[i] = pairOfChan{inputChan: make(chan int), outputChan: make(chan int)}
-		} else {
-			array[i] = pairOfChan{inputChan: array[i-1].outputChan, outputChan: make(chan int)}
-		}
+		//if i == 0 {
+		array[i] = pairOfChan{inputChan: make(chan int), outputChan: make(chan int), outputHaltChan: make(chan bool)}
+		/*} else {
+			array[i] = pairOfChan{inputChan: array[i-1].outputChan, outputChan: make(chan int), outputHaltChan: make(chan bool)}
+		}*/
 	}
 	return array
 }
@@ -191,7 +209,84 @@ func maxAmplifiersValue(programCode []int) int {
 	return *max
 }
 
+//solve part 2
+func maxAmplifiersValueUsingFeedbackLoog(programCode []int) int {
+	var max *int
+	for _, phaseSettingValues := range permutations([]int{5, 6, 7, 8, 9}) /* [][]int{[]int{5, 6, 7, 8, 9}}*/ {
+		var amplifierValue int = amplifierValueUsingFeedbackLoop(programCode, phaseSettingValues)
+		//fmt.Printf(" treating with value %+v \n", phaseSettingValues)
+		if max == nil || amplifierValue > *max {
+			max = &amplifierValue
+		}
+
+	}
+	if max == nil {
+		panic("No max found")
+	}
+	return *max
+}
+
+func amplifierValueUsingFeedbackLoop(programCode []int, phaseSettingValues []int) int {
+	amplifierValues := make([]int, 0)
+	amplifierValues = append(amplifierValues, 0)
+	//use chan to simulate signal input and output of amplifier
+	//each amplifier is represented by a goroutine and the signal mechanism is simulate by chan
+	//an output value of the current chan is the input value of the next amplifier input chan
+	//Because reading from a chan and writing to a chan block when the other side of the chan is not
+	//ready, this mechanism naturaly synchronise amplifiers (the first amplifier execute, output a value, this value is sent ,
+	//the second amplifer and son on)
+	var arrayInputOutputChan []pairOfChan = initializePairOfChan(len(phaseSettingValues))
+
+	for ampID, phaseSetting := range phaseSettingValues {
+		//launch amplifier
+		go diagnosticCode(programCode, arrayInputOutputChan[ampID].inputChan, arrayInputOutputChan[ampID].outputChan,
+			arrayInputOutputChan[ampID].outputHaltChan, ampID)
+		//provide input signal of the current amplifier
+		arrayInputOutputChan[ampID].inputChan <- phaseSetting
+		arrayInputOutputChan[ampID].inputChan <- amplifierValues[len(amplifierValues)-1]
+		//get the output signal of the current amplifier and save this value the use it as the input signal of the next
+		//amplifier
+		amplifierValues = append(amplifierValues, <-arrayInputOutputChan[ampID].outputChan)
+
+		//For the moment, we don't care if the amplifier halt or not, put this code to not have a deadlock (ie : the
+		//amplifier send a halt value and we don't read it)
+		select {
+		case <-arrayInputOutputChan[ampID].outputHaltChan:
+		default:
+			// pass throught to not block when no halt value is sended over arrayInputOutputChan[ampID].outputHaltChan
+		}
+
+	}
+	//feedback loop, each output signal of the current amplifier is send to next amplifier
+	//until the last amplifier halt.
+	ampID := 0
+	lastAmplifierHalt := false
+	for !lastAmplifierHalt {
+		arrayInputOutputChan[ampID].inputChan <- amplifierValues[len(amplifierValues)-1]
+		//get the output signal of the current amplifier and save this value the use it as the input signal of the next
+		//amplifier
+		amplifierValues = append(amplifierValues, <-arrayInputOutputChan[ampID].outputChan)
+		select {
+		case halt := <-arrayInputOutputChan[ampID].outputHaltChan:
+			//fmt.Printf("amplifier %d died ? %+v \n", ampId, halt)
+			if halt && ampID == len(phaseSettingValues)-1 {
+				lastAmplifierHalt = true
+			}
+		default:
+			// pass throught to not block when the halt value has not yet been sended
+			// over arrayInputOutputChan[ampID].outputHaltCha
+		}
+		ampID++
+		if ampID == len(phaseSettingValues) {
+			ampID = 0
+		}
+
+	}
+	return amplifierValues[len(amplifierValues)-1]
+}
+
 func main() {
 	input := []int{3, 8, 1001, 8, 10, 8, 105, 1, 0, 0, 21, 30, 47, 64, 81, 98, 179, 260, 341, 422, 99999, 3, 9, 1001, 9, 5, 9, 4, 9, 99, 3, 9, 1002, 9, 5, 9, 101, 4, 9, 9, 102, 2, 9, 9, 4, 9, 99, 3, 9, 102, 3, 9, 9, 101, 2, 9, 9, 1002, 9, 3, 9, 4, 9, 99, 3, 9, 1001, 9, 5, 9, 1002, 9, 3, 9, 1001, 9, 3, 9, 4, 9, 99, 3, 9, 1002, 9, 3, 9, 101, 2, 9, 9, 102, 5, 9, 9, 4, 9, 99, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 99, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 99, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 99, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 99, 3, 9, 1001, 9, 2, 9, 4, 9, 3, 9, 101, 2, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1001, 9, 1, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 101, 1, 9, 9, 4, 9, 3, 9, 1002, 9, 2, 9, 4, 9, 3, 9, 102, 2, 9, 9, 4, 9, 99}
-	fmt.Printf("Part one, highest signal %d", maxAmplifiersValue(input))
+	fmt.Printf("Part one, highest signal %d \n", maxAmplifiersValue(input))
+	fmt.Printf("Part two, highest signal %d \n", maxAmplifiersValueUsingFeedbackLoog(input))
 }
